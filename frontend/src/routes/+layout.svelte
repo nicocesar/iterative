cecb531923689ff545c12a5c7afa51c3fc0f8e87
Nico Cesar <nico@nicocesar.com>
2025-08-29 12:50:09 -0400

    blank svelte project with version.json as the iteration

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import version from '../../version.json?raw';
+	const versionData = JSON.parse(version);
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
+
+iterative version {versionData.iteration}
